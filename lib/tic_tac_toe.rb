fb6d8c22298815bda64ebbf6c16379a363f2e4fe
Randi Nash <randidena@live.com>
2017-09-30 23:18:53 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -22,8 +22,8 @@ def input_to_index(letsgo)
   letsgo.to_i - 1
 end
 
-def move(board, position)
-  board[position]
+def move(board, position, character="X")
+  board[position]=character
 end
 
 def position_taken?(board, index)
