7de89790ed60def1b292e8ed2f165c7ed00c0e8c
Randi Nash <randidena@live.com>
2017-09-30 23:00:54 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -17,3 +17,10 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+def input_to_index(letsgo)
+  letsgo.to_i - 1
+end
+def move(board, position, character="X")
+  board[position]=character
+end
